c4e7a09774ad2eaaeda37b5cdc94f42f6c93a251
Josh Stoik <josh@blobfolio.com>
2022-12-27 11:16:12 -0800

    misc: bench end matches

--- benches/fn_trim_slice.rs
+++ benches/fn_trim_slice.rs
@@ -44,8 +44,16 @@ benches!(
 	Bench::spacer(),
 
 	Bench::new("&[u8]::trim_start_matches()")
-		.run(|| BYTES.trim_start_matches(|b| matches!(b, b'\t' | b' ' | b'\n' | b'H' | b'e'))),
+		.run(|| BYTES.trim_start_matches(|b| b.is_ascii_whitespace() || matches!(b, b'H' | b'e'))),
 
 	Bench::new("&str::trim_start_matches()")
-		.run(|| STR.trim_start_matches(|c| matches!(c, '\t' | ' ' | '\n' | 'H' | 'e'))),
+		.run(|| STR.trim_start_matches(|c: char| c.is_ascii_whitespace() || matches!(c, 'H' | 'e'))),
+
+	Bench::spacer(),
+
+	Bench::new("&[u8]::trim_end_matches()")
+		.run(|| BYTES.trim_end_matches(|b| b.is_ascii_whitespace() || matches!(b, b'd' | b'!'))),
+
+	Bench::new("&str::trim_end_matches()")
+		.run(|| STR.trim_end_matches(|c: char| c.is_ascii_whitespace() || matches!(c, 'd' | '!'))),
 );
